f3a3dce30036b9bbac6e6beaa979778cf6662fdd
Ed L <el@howit.is>
2018-01-09 01:40:56 -0800

    Cleanup main: buttons don't work because not wired

--- demo-app.lisp
+++ demo-app.lisp
@@ -112,87 +112,24 @@
         [button set-target :pointer result]
         [button set-action :pointer do-magic]))))
 
-(defparameter *main-task* nil)
 (defun main ()
-  ;; (break)
-  (setf *main-task*
-        (trivial-main-thread:with-body-in-main-thread (:blocking t)
-          (with-selectors ((shared-application "sharedApplication")
-                           (process-info "processInfo")
-                           (process-name "processName")
-                           (set-activation-policy "setActivationPolicy:")
-                           ;; (init-with-content-rect "initWithContentRect:styleMask:backing:defer:")
-                           (set-title "setTitle:")
-                           (run "run")
-                           (activate-ignoring-other-apps "activateIgnoringOtherApps:")
-                           (make-key-and-order-front "makeKeyAndOrderFront:")
-                           (cascade-top-left-from-point "cascadeTopLeftFromPoint:")
-                           (add-item "addItem:")
-                           (set-main-menu "setMainMenu:")
-                           (init-with-title "initWithTitle:action:keyEquivalent:")
-                           (set-submenu "setSubmenu:")
-                           (init-with-encoding "initWithCString:length:")
-                           (content-view "contentView")
-                           (add-subview "addSubview:")
-                           (set-target "setTarget:")
-                           (set-action "setAction:")
-                           terminate?
-                           ;; (application-should-terminate "applicationShouldTerminate:")
-                           ;; (set-delegate "setDelegate:")
-                           ;; (finish-launching "finishLaunching")
-                           alloc new autorelease
-                           )
-            [#@NSAutoReleasePool new]
-            [#@NSApplication shared-application]
-            [objc-runtime::ns-app set-activation-policy :int 0]
+  (trivial-main-thread:with-body-in-main-thread (:blocking t)
+    [#@NSAutoReleasePool @(new) ]
+    [#@NSApplication @(sharedApplication) ]
+    [objc-runtime::ns-app @(setActivationPolicy:) :int 0]
 
-            (let ((app-delegate (objc-runtime::objc-allocate-class-pair #@NSObject
-                                                                        "AppDelegate"
-                                                                        0)))
-              (objc-runtime::add-pointer-ivar app-delegate "window")
-              (objc-runtime::add-pointer-ivar app-delegate "delegate"))
+    (let ((app-delegate (objc-runtime::objc-allocate-class-pair
+                         #@NSObject "AppDelegate" 0)))
+      (objc-runtime::add-pointer-ivar app-delegate "window")
+      (objc-runtime::add-pointer-ivar app-delegate "delegate"))
 
-            (let* ((bundle [#@NSBundle @(mainBundle)])
-                   (nib [[#@NSNib @(alloc)] @(initWithNibNamed:bundle:)
-                        :pointer @"MainMenu"
-                        :pointer bundle]))
-              (cffi:with-foreign-object (p :pointer)
-                [nib @(instantiateWithOwner:topLevelObjects:)
-                :pointer objc-runtime::ns-app
-                :pointer p]))
-            [ objc-runtime::ns-app activate-ignoring-other-apps :boolean t]
-            [ objc-runtime::ns-app run]
-            ;; (break)
-            #+null
-            (let* ((application-name [[#@NSProcessInfo process-info] process-name]))
-              (let* ((menubar [[#@NSMenu new] autorelease])
-                     (app-menu-item [[#@NSMenuItem new] autorelease])
-                     (app-menu [[#@NSMenu new] autorelease])
-                     (quit-name [[#@NSString alloc] init-with-encoding :string "Quit" :uint 4])
-                     (key [[#@NSString alloc] init-with-encoding :string "q" :uint 1])
-                     (quit-menu-item
-                      [[[#@NSMenuItem alloc] init-with-title :pointer quit-name :pointer terminate? :string key] autorelease]))
-                [menubar add-item :pointer app-menu-item]
-                [app-menu add-item :pointer quit-menu-item]
-                [app-menu-item set-submenu :pointer app-menu]
-                [objc-runtime::ns-app set-main-menu :pointer menubar] )
-
-              (with-point (p (20 20))
-                (let* ((foreign-rect (make-rect 10 10 120 120))
-                       (the-window (init-window [#@NSWindow alloc] foreign-rect 1 2 nil))
-                       (the-button
-                        (init-with-frame [#@NSButton alloc] (make-rect 10 10 100 35)))
-                       (the-button1
-                        (init-with-frame [#@NSButton alloc] (make-rect 10 55 100 35)))
-                       )
-                  
-                  [the-button set-title :pointer @"Click me!"]
-                  (make-button-delegate the-button)
-                  (make-button-delegate the-button1)
-                  [(value-for-key the-window "contentView") add-subview :pointer the-button]
-                  [(value-for-key the-window "contentView") add-subview :pointer the-button1]
-                  [the-window cascade-top-left-from-point :pointer p]
-                  [the-window set-title :pointer application-name]
-                  [the-window make-key-and-order-front :pointer (cffi:null-pointer)]
-                  [ objc-runtime::ns-app activate-ignoring-other-apps :boolean t]
-                  [ objc-runtime::ns-app run])))))))
+    (let* ((bundle [#@NSBundle @(mainBundle)])
+           (nib [[#@NSNib @(alloc)] @(initWithNibNamed:bundle:)
+                                    :pointer @"MainMenu"
+                                    :pointer bundle]))
+      (cffi:with-foreign-object (p :pointer)
+        [nib @(instantiateWithOwner:topLevelObjects:)
+            :pointer objc-runtime::ns-app
+            :pointer p]))
+    [objc-runtime::ns-app @(activateIgnoringOtherApps:) :boolean t]
+    [objc-runtime::ns-app @(run) ]))
